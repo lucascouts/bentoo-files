2c085c1ffc09433f9d9e42c644f9087701b13453
lucascouts <lucascs@protonmail.com>
2019-01-14 02:37:18 -0300

    Bentoo changed to plasma profile, added Xfce profile and few updates on world and portage folder files

---
+++ xfce/installation/scripts/packages-fix.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# emerge options.
+EMERGE_DEFAULT_OPTS="--quiet-build=y --nodeps --oneshot --usepkg" 
+
+# short variable.
+OPTS=$EMERGE_DEFAULT_OPTS
+
+# list of packages to install.
+packages="pam dnscrypt-proxy"
+
+#
+ln -sf /etc/X11/xinit/xinitrc /root/.xinitrc
+
+# add the temporary directory of the binaries. 
+echo -e '\nPKGDIR="/installation/packages"' >> /etc/portage/make.conf
+
+# command to install the packages listed.
+FEATURES="-getbinpkg" emerge $OPTS $packages
+
+# remove the temporary directory line.
+sed -i '/PKGDIR/d' /etc/portage/make.conf
+
+# namserver configuration default file.
+echo -e 'resolv_conf=/etc/resolv.conf\nname_servers=127.0.0.1' > /etc/resolvconf.conf
+
+# namserver configuration file.
+echo 'nameserver 127.0.0.1' > /etc/resolv.conf
+
+# enable dnscrypt-proxy service on default init.
+rc-update add dnscrypt-proxy default && rc-service dnscrypt-proxy start
+
+# message.
+echo -e "\n >>> Sucessfull Installed! \n"
