25ec2676b5dfdd0ebd494df0e4e4e0f651d4b4ef
lucascouts <lucascs@protonmail.com>
2019-02-01 21:05:39 -0300

    update and fix minor

--- xfce/installation/scripts/packages-fix.sh
+++ xfce/installation/scripts/packages-fix.sh
@@ -7,16 +7,18 @@ EMERGE_DEFAULT_OPTS="--quiet-build=y --nodeps --oneshot --usepkg"
 OPTS=$EMERGE_DEFAULT_OPTS
 
 # list of packages to install.
-packages="pam dnscrypt-proxy"
+#packages="pam"
 
 #
 ln -sf /etc/X11/xinit/xinitrc /root/.xinitrc
 
-# add the temporary directory of the binaries. 
+# add the temporary directory of the binaries.
 echo -e '\nPKGDIR="/installation/packages"' >> /etc/portage/make.conf
 
 # command to install the packages listed.
-FEATURES="-getbinpkg" emerge $OPTS $packages
+FEATURES="-getbinpkg" emerge $OPTS pam &
+
+FEATURES="-getbinpkg" emerge $OPTS dnscrypt-proxy
 
 # remove the temporary directory line.
 sed -i '/PKGDIR/d' /etc/portage/make.conf
@@ -28,7 +30,4 @@ echo -e 'resolv_conf=/etc/resolv.conf\nname_servers=127.0.0.1' > /etc/resolvconf
 echo 'nameserver 127.0.0.1' > /etc/resolv.conf
 
 # enable dnscrypt-proxy service on default init.
-rc-update add dnscrypt-proxy default && rc-service dnscrypt-proxy start
-
-# message.
-echo -e "\n >>> Sucessfull Installed! \n"
+rc-update add dnscrypt-proxy default && rc-service dnscrypt-proxy restart
